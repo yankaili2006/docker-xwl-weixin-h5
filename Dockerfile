a53c8497b4e0575a853e14ddc852a1fc1271f52d
yankaili2006 <yankaili2006@163.com>
2017-01-15 22:32:38 +0800

    tomcat8

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,18 @@
-FROM java:8
+FROM dordoka/tomcat
 MAINTAINER coola58 "yankaili2006@gmail.com"
 
-ENV APP_HOME /myapp
+ENV APP_HOME /opt/tomcat
 RUN mkdir -p $APP_HOME
 WORKDIR $APP_HOME
 
 #update system timezone
 RUN ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-
+ 
 #update application timezone
 RUN echo "Asia/Shanghai" >> /etc/timezone
 
+
+ADD server.xml $APP_HOME/conf/server.xml
 ADD run.sh $APP_HOME/run.sh
 
 CMD ["sh", "run.sh"]
--- run.sh
+++ run.sh
@@ -4,12 +4,18 @@ set -x
 
 APP=xwl-weixin
 VERSION=1.0-SNAPSHOT
-JAR=$APP-$VERSION-jar-with-dependencies.jar
+WAR=$APP-$VERSION.war
 
-JAR_PATH=$(curl "http://101.200.176.31:8181/nexus/service/local/artifact/maven/resolve?r=snapshots&g=com.xunwulian&a=$APP&v=$VERSION&c=jar-with-dependencies" | grep repositoryPath | sed 's/\s*<[^>]*>//g')
+WAR_PATH=$(curl "http://101.200.176.31:8181/nexus/service/local/artifact/maven/resolve?r=snapshots&g=com.xunwulian&a=$APP&v=$VERSION&p=war" | grep repositoryPath | sed 's/\s*<[^>]*>//g')
 
-NEXUS="http://101.200.176.31:8181/nexus/content/repositories/snapshots/$JAR_PATH"
+NEXUS="http://101.200.176.31:8181/nexus/content/repositories/snapshots/$WAR_PATH"
 
-wget $NEXUS -O $JAR
+wget $NEXUS -O $WAR
 
-java -jar $JAR -a zxtd | tee log.txt
+mkdir -p $APP
+
+unzip $WAR -o -d $APP
+
+sh ./bin/startup.sh
+
+tail -f logs/catalina.out

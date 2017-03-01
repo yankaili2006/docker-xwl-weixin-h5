ec829d85a6d09ff9235d8f0f58b8b760fb17319a
yankaili2006 <yankaili2006@163.com>
2017-03-01 21:32:50 +0800

    update

--- run.sh
+++ run.sh
@@ -6,9 +6,9 @@ APP=xwl-weixin-h5
 VERSION=1.0-SNAPSHOT
 WAR=$APP-$VERSION.war
 
-WAR_PATH=$(curl "http://101.200.176.31:8181/nexus/service/local/artifact/maven/resolve?r=snapshots&g=com.xunwulian&a=$APP&v=$VERSION&p=war" | grep repositoryPath | sed 's/\s*<[^>]*>//g')
+WAR_PATH=$(curl "http://101.200.176.31:8082/nexus/service/local/artifact/maven/resolve?r=snapshots&g=com.xunwulian&a=$APP&v=$VERSION&p=war" | grep repositoryPath | sed 's/\s*<[^>]*>//g')
 
-NEXUS="http://101.200.176.31:8181/nexus/content/repositories/snapshots/$WAR_PATH"
+NEXUS="http://101.200.176.31:8082/nexus/content/repositories/snapshots/$WAR_PATH"
 
 wget $NEXUS -O $WAR
 
